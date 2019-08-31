49e8ab2237a567c2b1e5aeecb4387d5480729da6
RKNyra <rk.nyra@gmail.com>
2019-08-31 21:57:23 +0300

    add a slider/carousel effect on the content images to add visual appeal

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-    $(".welcome img.content-img1").mouseover(function() {
-        $(this).toggle("2000ms");
-    });
-    $(".test1").hover(function() {
-        $(".test2").toggle();
+    $(".content1").mouseenter(function() {
+        $(".contant1").toggle("slow");
+        $(".content2").toggle("5000ms");
     });
 });
