acb54ab909cc6fd130893124a6144f5b856e307c
RKNyra <rk.nyra@gmail.com>
2019-09-02 04:03:59 +0300

    create arrays for pizza sizes and crust options for use in subsequent total pizza price calculations

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,34 @@ $(document).ready(function() {
     //     $(".contant1").toggle("slow");
     //     $(".content2").toggle("5000ms");
     // });
+
+
+    //Busines Logic
+    const pizzaSizeArray = [{
+            name: "Extra Large",
+            price: 900
+        },
+        {
+            name: "Large",
+            price: 700
+        },
+        {
+            name: "Medium",
+            price: 500
+        },
+        {
+            name: "Small",
+            price: 300
+        }
+    ]
+
+    const crustOptionArray = [{
+            name: "Double-Delight",
+            price: 300
+        },
+        {
+            name: "Regular",
+            price: 0
+        }
+    ]
 });
