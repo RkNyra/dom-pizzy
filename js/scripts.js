c6a74d5c1bf10f61272cec4fe52326fe8c334eb0
RKNyra <rk.nyra@gmail.com>
2019-09-02 06:34:33 +0300

    add functions that enable the pizza order summary to be displayed after a clietn clicks the checkout button

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     // });
 
 
-    //Busines Logic
+
     const pizzaSizeArray = [{
             name: "Extra Large",
             price: 900
@@ -33,50 +33,120 @@ $(document).ready(function() {
             price: 0
         }
     ]
+
+    const toppingsOptionArray = [{
+            name: "BBQ Chicken",
+            price: 300
+        },
+        {
+            name: "Meat Deluxe",
+            price: 200
+        },
+        {
+            name: "Margherita",
+            price: 250
+        }
+    ]
+
+
+
+
+    $("#orderPizza").submit(function(event) {
+        event.preventDefault();
+
+        pizzaSize = $("#pizzaSize").val();
+        crustOption = $("#crustOption").val();
+        toppingsOption = $("#toppings").val();
+        extraToppings = $("#extraToppings").val();
+
+
+        numberOfOrder = $("#numberOfOrders").val();
+        deliveryPoint = $("#deliveryPoint").val();
+
+
+        let pizzaSizePrice = 0;
+        let crustOptionPrice = 0;
+        let toppingsOptionPrice = 0;
+        let extraToppingsPrice = 0;
+        //to use these variables later
+
+
+
+        //find out which size was selected
+
+        $.each(pizzaSizeArray, function(key, value) {
+            if (value.name == pizzaSize) {
+                console.log(value.name + value.price);
+                pizzaSizePrice = value.price;
+                return pizzaSizePrice
+            }
+        });
+
+
+        //find out which crust was selected
+        console.log(pizzaSizePrice)
+        $.each(crustOptionArray, function(key, value) {
+            if (value.name == crustOption) {
+                console.log(value.name + value.price);
+                crustOptionPrice = value.price;
+                return crustOptionPrice
+            }
+        });
+
+
+        //find out which topping was selected
+        $.each(toppingsOptionArray, function(key, value) {
+            if (value.name == toppingsOption) {
+                console.log(value.name + value.price);
+                toppingsOptionPrice = value.price;
+                return toppingsOptionPrice
+            }
+        });
+
+
+        console.log(extraToppings)
+        if (extraToppings == "Familiar") {
+            extraToppingsPrice = 0;
+            console.log(extraToppingsPrice);
+        } else {
+            if (extraToppings == "Surprise Me" && pizzaSize == "Extra Large") {
+                extraToppingsPrice = 250;
+                console.log(extraToppingsPrice);
+            } else if (extraToppings == "Surprise Me" && pizzaSize == "Large") {
+                extraToppingsPrice = 200;
+                console.log(extraToppingsPrice);
+            } else if (extraToppings == "Surprise Me" && pizzaSize == "Medium") {
+                extraToppingsPrice = 150;
+                console.log(extraToppingsPrice);
+            } else if (extraToppings == "Surprise Me" && pizzaSize == "Small") {
+                extraToppingsPrice = 100;
+                console.log("extraToppings ", extraToppingsPrice)
+            }
+        }
+
+
+
+        //total price calculation (size, crust, toppings, extraToppings)
+        const totalCostOfPizza = pizzaSizePrice + crustOptionPrice + toppingsOptionPrice + extraToppingsPrice;
+        console.log("total ", totalCostOfPizza);
+
+
+        //total with number of orders
+
+        const finalTotal = totalCostOfPizza * numberOfOrder;
+
+
+
+        console.log(finalTotal);
+
+        $("#sizeAndPrice").html("Size and Price: " + pizzaSizePrice);
+        $("#crustAndPrice").html("Crust and Price: " + crustOptionPrice);
+        $("#toppingsAndPrice").html("Toppings and Price: " + toppingsOptionPrice);
+        $("#extraToppingsAndPrice").html("Extra Toppings and Price: " + extraToppingsPrice);
+        $("#subTotal").html("Subtotal Cost: " + totalCostOfPizza);
+        $("#orderNumber").html("Number of Order(s): " + numberOfOrder);
+        $("#grandTotal").html("Grand Total Cost: " + finalTotal);
+
+    });
+
 });
-
-
-
-//UI-Logic
-$("#orderPizza").submit(function(event) {
-    event.preventDefault();
-
-    pizzaSize = $("#pizzaSize").val();
-    crustOption = $("#crustOption").val();
-    toppings = $("#toppings").val();
-
-    numberOfOrder = $("#numberOfOrders").val();
-    deliveryPoint = $("#deliveryPoint").val();
-
-    //Business Logic
-    let pizzaSizePrice = 0;
-    let crustOptionPrice = 0;
-    let toppingsPrice = 0;
-    //to use these variables later
-
-
-
-    //find out which size was selected
-    console.log(pizzaSize)
-    $.each(pizzaSizeArray, function(key, value) {
-        if (value.name == pizzaSize) {
-            console.log("name " + value.name + " price " + value.price);
-            pizzaSizePrice == value.price;
-            return pizzaSizePrice
-        }
-    });
-
-
-    //find out which crust was selected
-    console.log(crustOption)
-    $.each(crustOptionArray, function(key, value) {
-        if (value.name == crustOption) {
-            console.log("name " + value.name + " price " + value.price);
-            crustOptionPrice == value.price;
-            return crustOptionPrice
-        }
-    });
-
-
-
-})
